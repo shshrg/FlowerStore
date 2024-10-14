caa93888104c4e143bb5c815db666f56f165e92c
Oleksandra Shergina <sasha.shergina@gmail.com>
2024-10-14 16:36:06 +0300

    Added the FlowerBucket class

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -3,4 +3,8 @@ package flower.store;
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+
+    public double getPrice() {
+        return flower.getPrice() * quantity;
+    }
 }
